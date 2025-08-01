78f7cc921e000cadb548a987d0b27efe3b7d225a
lidiia-tsymborovych <lidacimbirovic@gmail.com>
2025-08-01 22:54:28 +0300

    fix: small cleanup

--- app/components/home/NotFoundUI.tsx
+++ app/components/home/NotFoundUI.tsx
@@ -60,20 +60,6 @@ export function NotFoundUI() {
       >
         Take Me Home
       </button>
-
-      <style jsx>{`
-        @keyframes gradientBackground {
-          0% {
-            background-position: 0% 50%;
-          }
-          50% {
-            background-position: 100% 50%;
-          }
-          100% {
-            background-position: 0% 50%;
-          }
-        }
-      `}</style>
     </div>
   );
 }
